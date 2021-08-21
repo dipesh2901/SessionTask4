dddc0635b6ccfb85657fc8d921d555167bc02475
dipesh2901 <67797922+dipesh2901@users.noreply.github.com>
2021-08-21 13:45:17 +0530

    Program33- RequestDispatcher with Database

---
+++ JDBCConnection.java
@@ -0,0 +1,34 @@
+package com.neosoft.Task.program33;
+
+import java.sql.*;
+
+public class JDBCConnection {
+	static Connection con = null;
+
+	public static Connection openConnection() {
+		try {
+			JDBCProperty.loadProperties();
+			Class.forName(JDBCProperty.driverClass);
+			System.out.println("Driver Loaded!!!");
+			con = DriverManager.getConnection(JDBCProperty.url, JDBCProperty.username, JDBCProperty.password);
+			System.out.println("Connection established");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public static void closeConnection() {
+		try {
+			if(con!=null) {
+				con.close();
+				System.out.println("Connection Closed!!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+}
