1ddd778877710a115df9b1ed5c3cfbc8192e5f59
dipesh2901 <67797922+dipesh2901@users.noreply.github.com>
2021-08-21 13:44:29 +0530

    Program33- RequestDispatcher with Database

---
+++ UserRegistration.java
@@ -0,0 +1,35 @@
+package com.neosoft.Task.program33;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = "/registerUser")
+public class UserRegistration extends HttpServlet {
+@Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	resp.setContentType("text/html");
+	PrintWriter out=resp.getWriter();
+	String emailid,mobile,username,password;
+	int id;
+	id=Integer.parseInt(req.getParameter("uid"));
+	emailid=req.getParameter("uemail");
+	mobile=req.getParameter("umobile");
+	username=req.getParameter("username");
+	password=req.getParameter("pass");
+	UserInfo.insertUserInfo(new UserData(id,emailid,mobile,username,password));
+	out.print("Success!!!");
+	RequestDispatcher rd=req.getRequestDispatcher("index.html"); 
+	rd.forward(req,resp);
+	
+	
+}
+}
